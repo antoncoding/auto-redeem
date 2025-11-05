ce5dc7a288596d527839ad14d1845c3ca66af613
antoncoding <antonassocareer@gmail.com>
2025-11-05 15:37:17 +0800

    chore: init

---
+++ index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+async function main() {
+  console.log('Auto-redeem script starting...');
+
+  // Your code here
+
+  console.log('Script completed.');
+}
+
+main().catch((error) => {
+  console.error('Error:', error);
+  process.exit(1);
+});
